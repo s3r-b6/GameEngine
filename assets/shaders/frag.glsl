239f2f2f361b4b1ff860f9a4cd14caec0ffafa4f
Sergio Bermejo de las Heras <sergio.bermejo123@gmail.com>
2024-05-13 15:17:08 +0200

    playing around with the CRT idea...

--- assets/shaders/frag.glsl
+++ assets/shaders/frag.glsl
@@ -13,14 +13,10 @@ layout(binding=3) uniform sampler2D textureAtlas3;
 
 uniform vec2 screenSize;
 
-float max(vec3 vec){
-    if(vec.r>vec.g){
-        if(vec.r>vec.b) return vec.r;
-        else return vec.b;
-    }else{
-        if(vec.g>vec.b) return vec.g;
-        else return vec.b;
-    }
+void saturate(inout vec4 vec) {
+    float avg = (vec.r + vec.g + vec.b) / 3.0;
+    vec.rgb += (vec.rgb - avg) * 0.11;
+    vec = clamp(vec, 0.0, 1.0);
 }
 
 void main() {
@@ -29,24 +25,23 @@ void main() {
     if(textureAtlasIdx == 0){
         float red = texelFetch(fontAtlas, ivec2(textureCoordsIn), 0).r;
         
-        if(red == 0.f) discard;
+        if(red == 0.0) discard;
         
-        textureColor = red * vec4(1.f, 1.f, 1.f, 1.f);
+        textureColor = vec4(red);
         fragColor = textureColor;
         return;
     }
     
-    if(textureAtlasIdx == 1) textureColor = texelFetch(textureAtlas1, ivec2(textureCoordsIn), 0);
-    else if(textureAtlasIdx == 2) textureColor = texelFetch(textureAtlas2, ivec2(textureCoordsIn), 0);
-    else if(textureAtlasIdx == 3) textureColor = texelFetch(textureAtlas3, ivec2(textureCoordsIn), 0);
+    if (textureAtlasIdx == 1) textureColor = texelFetch(textureAtlas1, ivec2(textureCoordsIn), 0);
+    else if (textureAtlasIdx == 2) textureColor = texelFetch(textureAtlas2, ivec2(textureCoordsIn), 0);
+    else if (textureAtlasIdx == 3) textureColor = texelFetch(textureAtlas3, ivec2(textureCoordsIn), 0);
     else discard;
-    
-    if(textureColor.a == 0.f) discard;
-    int row = int(textureCoordsIn.y * screenSize.y);
-    if (mod(row, 8) < 4) {
-        fragColor = mix(vec4(0.0), textureColor, 0.5); 
-    } else {
-        fragColor = textureColor;
-    }
+
+    if (textureColor.a == 0.0) discard;
+
+    fragColor = textureColor;
+    fragColor.g *= (sin(gl_FragCoord.y * 0.7) + 1.0) * 0.15 + 1.0;
+    fragColor.rb *= (cos(gl_FragCoord.y  * 0.7) + 1.0) * 0.135 + 1.0;
+    saturate(fragColor);
 }
 
